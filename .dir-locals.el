2dc73372bb4f12041e0a8abc0627eb1b093053c3
Bojun Ren <me.rennsax@gmail.com>
2026-03-05 14:43:18 +0800

    chore: update .dir-locals.el for Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((markdown-mode . ((eval . (add-hook 'before-save-hook #'time-stamp t))
-                   (time-stamp-pattern . "10/^last_modified_at: %Y-%m-%d %H:%M:%S %5z$"))))
+                   (time-stamp-pattern . "20/^last_modified_at: %Y-%m-%d %H:%M:%S %5z$"))))
